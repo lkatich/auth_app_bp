ae9ec163b5be9bdb744a612045d5c9d19cc501d5
lkaticheva <55089735+lkatich@users.noreply.github.com>
2023-07-20 17:38:17 +0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -8,25 +8,11 @@ DATABASE = {
     'PASSWORD': os.environ['DB_PASSWORD'],
 }
 
-# DATABASE = {
-#     'HOST': '127.0.0.1',
-#     'PORT': 5432,
-#     'NAME': 'bp_app',
-#     'USER': 'postgres',
-#     'PASSWORD': 'postgres',
-# }
-
 REDIS = {
     'HOST': os.environ['REDIS_HOST'],
     'PORT': os.environ['REDIS_PORT'],
     'DB': os.environ['REDIS_DB']
 }
-
-# REDIS = {
-#     'HOST': '127.0.0.1',
-#     'PORT': 6379,
-#     'DB': 0
-# }
 
 SECRET_KEY = "AuthAppSecretKey"
 
